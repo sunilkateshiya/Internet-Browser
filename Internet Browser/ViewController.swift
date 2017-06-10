272a7de3a263ef4809f2020d25667455c72cde96
iFlame <iflame@administrators-device.local>
2017-06-10 18:06:08 +0530

    commit

--- Internet Browser/ViewController.swift
+++ Internet Browser/ViewController.swift
@@ -8,53 +8,85 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITextFieldDelegate {
     
     @IBOutlet weak var url: UITextField!
     
     @IBOutlet weak var webView: UIWebView!
+    
+    @IBOutlet weak var google: UITextField!
+    
     override func viewDidLoad() {
+        
         webView.loadRequest(NSURLRequest(url:NSURL(string: "http://google.com")! as URL) as URLRequest)
         
-        
         super.viewDidLoad()
+        url.delegate = self
+        google.delegate = self
+    
+        
         
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    
     override func didReceiveMemoryWarning() {
+        
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
     
-    @IBAction func search(_ sender: UIButton) {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+         textField.resignFirstResponder()
         
-        webView.loadRequest(NSURLRequest(url: NSURL(string: "http://" + url.text! + "/")! as URL) as URLRequest)
-        if url.text == "" {
-            webView.loadRequest(NSURLRequest(url:NSURL(string: "http://google.com")! as URL) as URLRequest)
+        if url.text != "" {
+    
+        webView.loadRequest(NSURLRequest(url: NSURL(string: "https://" + url.text! + "/")! as URL) as URLRequest)
+            url.text = ""
+        }
+        
+        else if google.text != "" {webView.loadRequest(NSURLRequest(url: NSURL(string: "https://www.google.co.in/search?q=" + google.text! )! as URL) as URLRequest)
+            google.text = ""
             
         }
+        
+        
+      
+          textField.resignFirstResponder()
+        
+
+        return true
     }
     
     
     @IBAction func back(_ sender: UIButton) {
-        webView.goBack()
         
+        webView.goBack()
         
     }
     
     @IBAction func next(_ sender: UIButton) {
+        
         webView.goForward()
-    
+        
     }
-    
     
     @IBAction func home(_ sender: UIButton) {
         
-        webView.loadRequest(NSURLRequest(url:NSURL(string: "http://google.com")! as URL) as URLRequest)
         
+        webView.loadRequest(NSURLRequest(url:NSURL(string: "http://google.com")! as URL) as URLRequest)
+        url.text = ""
         
     }
     
+    @IBAction func reload(_ sender: UIButton) {
+        webView.reload()
+      
+     
+        
     }
+    
+    
+}
 
